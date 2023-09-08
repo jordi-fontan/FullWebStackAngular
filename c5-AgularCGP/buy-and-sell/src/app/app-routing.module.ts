86d7e13aa8ef7d2b51e1c7b58112f231a9dc9196
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-09-08 13:16:07 +0200

    Add Routes, styles, and fake data

---
+++ c5-AgularCGP/buy-and-sell/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes,RouterModule } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingsPageComponent } from './edit-listings-page/edit-listings-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
+
+const routes:Routes =[
+  {path:'',redirectTo:'/listings',pathMatch:'full'},
+  {path:'listings', component:ListingsPageComponent,pathMatch:'full'},
+  {path:'listings/:id', component:ListingDetailPageComponent},
+  {path:'contact/:id', component:ContactPageComponent},
+  {path:'edit-listing/:id', component:EditListingsPageComponent},
+  {path:'my-listings', component:MyListingsPageComponent},
+  {path:'new-listing', component:NewListingsPageComponent} 
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,RouterModule.forRoot(routes)],
+  exports: [RouterModule]  
+})
+export class AppRoutingModule { }
